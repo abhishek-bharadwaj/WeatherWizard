af7825564af8e4a5bb4a140f61c4123c124a8924
Abhishek Bharadwaj <abhishek.bharadwaj@healthify.co>
2018-09-23 17:01:14 +0530

    Fixed one crash

--- app/src/main/java/com/abhishek/weatherwizard/data/repository/room/WeatherData.kt
+++ app/src/main/java/com/abhishek/weatherwizard/data/repository/room/WeatherData.kt
@@ -3,18 +3,17 @@ package com.abhishek.weatherwizard.data.repository.room
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 
-@Entity(tableName = WEATHER_DATA_TABLE,
-    primaryKeys = [COLUMN_LAT, COLUMN_LON])
+@Entity(tableName = WEATHER_DATA_TABLE, primaryKeys = [COLUMN_LAT, COLUMN_LON])
 class WeatherData(
     @ColumnInfo(name = COLUMN_LAT)
-    val latitude: Double = 0.0,
+    var latitude: Double = 0.0,
     @ColumnInfo(name = COLUMN_LON)
-    val longitude: Double = 0.0,
+    var longitude: Double = 0.0,
     @ColumnInfo(name = COLUMN_NAME)
-    val name: String = "",
+    var name: String = "",
     @ColumnInfo(name = COLUMN_REGION)
-    val region: String = "",
+    var region: String = "",
     @ColumnInfo(name = COLUMN_COUNTRY)
-    val country: String = "",
+    var country: String = "",
     @ColumnInfo(name = COLUMN_CURRENT_TMP)
-    val currentTmp: Double = 0.0)
+    var currentTmp: Double = 0.0)
--- app/src/main/java/com/abhishek/weatherwizard/data/repository/room/WeatherDatabase.kt
+++ app/src/main/java/com/abhishek/weatherwizard/data/repository/room/WeatherDatabase.kt
@@ -1,9 +1,11 @@
 package com.abhishek.weatherwizard.data.repository.room
 
+import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import com.abhishek.weatherwizard.MyApplication
 
+@Database(entities = [WeatherData::class], version = 1)
 abstract class WeatherDatabase : RoomDatabase() {
 
     abstract fun weatherDataDao(): WeatherDataDao
